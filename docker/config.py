ffc40198b551a8f43b5e0fd7bc6395c79416ed8b
Bart Jeukendrup <bart@jeukendrup.nl>
2017-11-01 14:55:17 -0400

    update config.py

---
+++ docker/config.py
@@ -0,0 +1,35 @@
+import os
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG')
+
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOST')]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.getenv('DB_HOST'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'NAME': os.getenv('DB_NAME'),
+    }
+}
+
+STATIC_ROOT = '/app/static'
+
+LANGUAGE_CODE = 'nl-nl'
+
+TIME_ZONE = 'Europe/Amsterdam'
+
+STATIC_ROOT = '/app/static'
+
+CHECK_PREVIOUS_LOGINS = os.getenv('CHECK_PREVIOUS_LOGINS')
+
+FROM_EMAIL = os.getenv('FROM_EMAIL')
+
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL')
