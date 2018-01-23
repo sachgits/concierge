d76d331b4442f9af007db1f2f4e67aa5c06acfa7
Luc Belliveau <luc.belliveau@nrc-cnrc.gc.ca>
2018-01-23 16:50:55 -0400

    feat: Added group checking functionality

--- emailvalidator/tests_emailvalidator.py
+++ emailvalidator/tests_emailvalidator.py
@@ -2,7 +2,7 @@
 Registration form validation tests using emailvalidator app
 """
 from django.test import TestCase
-from .validator import is_email_valid
+from .validator import is_email_valid, get_email_groups, is_email_in_group
 
 
 class EmailValidatorAnyTestCase(TestCase):
@@ -38,3 +38,48 @@ class EmailValidatorGoCTestCase(TestCase):
         self.assertIs(is_email_valid("anyone@anywhere.net"), False)
         self.assertIs(is_email_valid("invalid@gc.ca"), False)
         self.assertIs(is_email_valid("invalid@dept.gc.g.ca"), False)
+
+
+class EmailValidatorGroupMatchTestCase(TestCase):
+    """
+    Run tests to determine group membership
+    """
+    fixtures = ['emailvalidator-any.json', 'emailvalidator-goc.json']
+
+    def test_group_count(self):
+        """
+        Test if returned group count matches expectations
+        """
+        groups = get_email_groups("testing@anywher.net")
+        self.assertIs(len(groups), 1)
+
+        groups = get_email_groups("someone@canada.ca")
+        self.assertIs(len(groups), 2)
+
+    def test_group_array(self):
+        """
+        Test if returned group array is correct
+        """
+        groups = get_email_groups("testing@anywhere.net")
+        self.assertEqual(groups[0].name, 'All')
+
+        groups = get_email_groups("someone@canada.ca")
+        for group in groups:
+            self.assertIn(group.name, ['Government of Canada', 'All'])
+
+    def test_group_membership(self):
+        """
+        Test group membership functions
+        """
+        self.assertIs(is_email_in_group("someone@canada.ca", "All"), True)
+        self.assertIs(is_email_in_group("someone@canada.ca", "Fake"), False)
+        self.assertIs(is_email_in_group(
+            "someone@canada.ca",
+            "Government of Canada"
+        ), True)
+        self.assertIs(is_email_in_group(
+            "someone@anywhere.net",
+            "Government of Canada"
+        ), False)
+        self.assertIs(is_email_in_group("someone@anywhere.net", "All"), True)
+
--- emailvalidator/validator.py
+++ emailvalidator/validator.py
@@ -2,7 +2,7 @@
 Initialize the emailvalidator django application, and configure
 """
 import re
-from .models import EmailRegExValidator
+from .models import EmailRegExValidator, EmailDomainGroup
 
 
 def is_email_valid(email):
@@ -13,3 +13,27 @@ def is_email_valid(email):
     validators = EmailRegExValidator.objects.all()
     regexes = [re.compile(r.regex) for r in validators]
     return any(regex.match(email) for regex in regexes)
+
+
+def get_email_groups(email):
+    """
+    Returns an array of groups the provided email address matches.
+    """
+    validators = EmailRegExValidator.objects.all()
+    group_regexes = [(re.compile(r.regex), r.group) for r in validators]
+    return [group for regex, group in group_regexes if regex.match(email)]
+
+
+def is_email_in_group(email, group):
+    """
+    Returns true if the specified email address successfully passes validation
+    for the named group.
+    """
+    try:
+        domain_group = EmailDomainGroup.objects.get(name=group)
+        validators = EmailRegExValidator.objects.filter(group=domain_group)
+        regexes = [re.compile(r.regex) for r in validators]
+        return any(regex.match(email) for regex in regexes)
+    except EmailDomainGroup.DoesNotExist:
+        return False
+
