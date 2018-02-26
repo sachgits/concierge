d141cb3d4ab065014e7110129bd175acd0757d01
Aad Schippers <aadschippers@gmail.com>
2018-02-26 15:33:58 +0100

    Made include_asset more fault-tolerant

--- core/templatetags/Pleio_templatetags.py
+++ core/templatetags/Pleio_templatetags.py
@@ -1,3 +1,5 @@
+import pathlib
+import os
 from django import template
 from django.conf import settings
 from django.utils.safestring import mark_safe
@@ -6,4 +8,12 @@ register = template.Library()
  
 @register.simple_tag
 def include_asset(file_name):
-    return mark_safe(open(settings.STATICFILES_DIRS[0]+file_name).read())
+    p = pathlib.Path(os.path.join(settings.STATICFILES_DIRS[0], file_name))
+
+    print(settings.STATICFILES_DIRS)
+
+    try:
+        with p.open() as f:
+            return mark_safe(f.read())
+    except OSError:
+        return None
