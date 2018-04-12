126a69cf1156690d03a92162bf13ab70eefd784d
Aad Schippers <aadschippers@gmail.com>
2018-04-12 07:40:32 +0200

    directory renamed, start.sh amended

--- docs/saml_idp_testing/start.sh
+++ docs/saml_idp_testing/start.sh
@@ -4,8 +4,8 @@ docker run --name=saml-idp-blue \
 -e SIMPLESAMLPHP_SP_ENTITY_ID=http://172.17.0.1:8000/saml/metadata/ \
 -e SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE=http://172.17.0.1:8000/saml/acs/ \
 -e SIMPLESAMLPHP_SP_SINGLE_LOGOUT_SERVICE=http://172.17.0.1:8000/saml/slo/ \
--v ~/saml_idp/test-saml-idp-user-blue.php:/var/www/simplesamlphp/config/authsources.php \
--v ~/saml_idp/gtk-dialog-authentication-blue.48x48.png:/var/www/simplesamlphp/www/resources/icons/experience/gtk-dialog-authentication.48x48.png \
+-v ~/concierge/docs/saml_idp_testing/test-saml-idp-user-blue.php:/var/www/simplesamlphp/config/authsources.php \
+-v ~/concierge/docs/saml_idp_testing/gtk-dialog-authentication-blue.48x48.png:/var/www/simplesamlphp/www/resources/icons/experience/gtk-dialog-authentication.48x48.png \
 -d kristophjunge/test-saml-idp
 
 docker run --name=saml-idp-green \
@@ -14,8 +14,8 @@ docker run --name=saml-idp-green \
 -e SIMPLESAMLPHP_SP_ENTITY_ID=http://172.17.0.1:8000/saml/metadata/ \
 -e SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE=http://172.17.0.1:8000/saml/acs/ \
 -e SIMPLESAMLPHP_SP_SINGLE_LOGOUT_SERVICE=http://172.17.0.1:8000/saml/slo/ \
--v ~/saml_idp/test-saml-idp-user-green.php:/var/www/simplesamlphp/config/authsources.php \
--v ~/saml_idp/gtk-dialog-authentication-green.48x48.png:/var/www/simplesamlphp/www/resources/icons/experience/gtk-dialog-authentication.48x48.png \
+-v ~/concierge/docs/saml_idp_testing/test-saml-idp-user-green.php:/var/www/simplesamlphp/config/authsources.php \
+-v ~/concierge/docs/saml_idp_testing/gtk-dialog-authentication-green.48x48.png:/var/www/simplesamlphp/www/resources/icons/experience/gtk-dialog-authentication.48x48.png \
 -d kristophjunge/test-saml-idp
 
 docker run --name=saml-idp-red \
@@ -24,8 +24,8 @@ docker run --name=saml-idp-red \
 -e SIMPLESAMLPHP_SP_ENTITY_ID=http://172.17.0.1:8000/saml/metadata/ \
 -e SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE=http://172.17.0.1:8000/saml/acs/ \
 -e SIMPLESAMLPHP_SP_SINGLE_LOGOUT_SERVICE=http://172.17.0.1:8000/saml/slo/ \
--v ~/saml_idp/test-saml-idp-user-red.php:/var/www/simplesamlphp/config/authsources.php \
--v ~/saml_idp/gtk-dialog-authentication-red.48x48.png:/var/www/simplesamlphp/www/resources/icons/experience/gtk-dialog-authentication.48x48.png \
+-v ~/concierge/docs/saml_idp_testing/test-saml-idp-user-red.php:/var/www/simplesamlphp/config/authsources.php \
+-v ~/concierge/docs/saml_idp_testing/gtk-dialog-authentication-red.48x48.png:/var/www/simplesamlphp/www/resources/icons/experience/gtk-dialog-authentication.48x48.png \
 -d kristophjunge/test-saml-idp
 
 docker run --name=saml-idp-yellow \
@@ -34,8 +34,8 @@ docker run --name=saml-idp-yellow \
 -e SIMPLESAMLPHP_SP_ENTITY_ID=http://172.17.0.1:8000/saml/metadata/ \
 -e SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE=http://172.17.0.1:8000/saml/acs/ \
 -e SIMPLESAMLPHP_SP_SINGLE_LOGOUT_SERVICE=http://172.17.0.1:8000/saml/slo/ \
--v ~/saml_idp/test-saml-idp-user-yellow.php:/var/www/simplesamlphp/config/authsources.php \
--v ~/saml_idp/gtk-dialog-authentication-yellow.48x48.png:/var/www/simplesamlphp/www/resources/icons/experience/gtk-dialog-authentication.48x48.png \
+-v ~/concierge/docs/saml_idp_testing/test-saml-idp-user-yellow.php:/var/www/simplesamlphp/config/authsources.php \
+-v ~/concierge/docs/saml_idp_testing/gtk-dialog-authentication-yellow.48x48.png:/var/www/simplesamlphp/www/resources/icons/experience/gtk-dialog-authentication.48x48.png \
 -d kristophjunge/test-saml-idp
 
 
