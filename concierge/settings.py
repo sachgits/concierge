270a41628ab149ea5019f5dc8af3a4c71fb519ec
Ilia <phanoix@gmail.com>
2018-04-25 02:13:29 -0400

    session cookie security patch (#46)
    SESSION_COOKIE_SECURE should be true not sure why this isn't the default considering what the docs have to say about it
    https://docs.djangoproject.com/en/2.0/ref/settings/#session-cookie-secure
    
    SESSION_COOKIE_HTTPONLY is true by default, this just declares it more explicitly

--- concierge/settings.py
+++ concierge/settings.py
@@ -108,6 +108,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'concierge.wsgi.application'
 
 SESSION_ENGINE = 'user_sessions.backends.db'
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_HTTPONLY = True
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
